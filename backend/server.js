505d6b75b76ac1dcab11acb07ad76fe6dac8c8ee
Hamza Asif <hmziiiraj@gmail.com>
2023-12-27 17:52:05 +0500

    added: cors for handling cors erros

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const express = require("express");
 const userRouter = require("./routes/user");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/user", userRouter);
